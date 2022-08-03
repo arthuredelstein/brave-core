a8753af3390ffbb67b296d2b00b7002d73ea7e4d
Arthur Edelstein <aedelstein@brave.com>
2022-08-03 16:15:45 -0700

    fix nits from bridiver

--- chromium_src/third_party/blink/renderer/core/page/create_window.cc
+++ chromium_src/third_party/blink/renderer/core/page/create_window.cc
@@ -5,7 +5,7 @@
 
 #include "third_party/blink/renderer/core/page/create_window.h"
 
-#include "brave/third_party/blink/renderer/farbling/brave_session_cache.h"
+#include "brave/third_party/blink/renderer/core/farbling/brave_session_cache.h"
 #include "third_party/blink/public/web/web_window_features.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
@@ -18,7 +18,7 @@
 // window size to that width and height as well, so that window.open can't
 // be used to probe the screen size.
 
-namespace blink {
+namespace {
 
 WebWindowFeatures MaybeFarbleWindowFeatures(const String& feature_string,
                                             LocalDOMWindow* dom_window) {
@@ -48,7 +48,7 @@ WebWindowFeatures MaybeFarbleWindowFeatures(const String& feature_string,
   return window_features;
 }
 
-}  // namespace blink
+}  // namespace
 
 #define GetWindowFeaturesFromString                               \
   GetWindowFeaturesFromString(const String& feature_string,       \

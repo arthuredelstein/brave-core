e6ef712979b308c27ded97d7d56d96c1650832f3
Arthur Edelstein <arthuredelstein@gmail.com>
2022-08-24 12:27:05 -0700

    RoundDOMHighResTimeStamp

---
+++ chromium_src/third_party/blink/renderer/core/dom/frame_request_callback_collection.cc
@@ -0,0 +1,28 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "third_party/blink/renderer/core/dom/frame_request_callback_collection.h"
+
+#define ExecuteFrameCallbacks ExecuteFrameCallbacks_ChromiumImpl
+
+#include "src/third_party/blink/renderer/core/dom/frame_request_callback_collection.cc"
+
+#undef ExecuteFrameCallbacks
+
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
+
+namespace blink {
+
+void FrameRequestCallbackCollection::ExecuteFrameCallbacks(
+    double high_res_now_ms,
+    double high_res_now_ms_legacy) {
+  if (!brave::AllowFingerprinting(context_)) {
+    high_res_now_ms = round(high_res_now_ms);
+    high_res_now_ms_legacy = round(high_res_now_ms_legacy);
+  }
+  ExecuteFrameCallbacks_ChromiumImpl(high_res_now_ms, high_res_now_ms_legacy);
+}
+
+}  // namespace blink
---
+++ chromium_src/third_party/blink/renderer/modules/video_rvfc/video_frame_callback_requester_impl.h
@@ -0,0 +1,18 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_VIDEO_RVFC_VIDEO_FRAME_CALLBACK_REQUESTER_IMPL_H_
+
+#define BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_VIDEO_RVFC_VIDEO_FRAME_CALLBACK_REQUESTER_IMPL_H_
+
+#define cross_origin_isolated_capability_ \
+  allow_fingerprinting_ = false;          \
+  bool cross_origin_isolated_capability_
+
+#include "src/third_party/blink/renderer/modules/video_rvfc/video_frame_callback_requester_impl.h"
+
+#undef cross_origin_isolated_capability_
+
+#endif  // BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_VIDEO_RVFC_VIDEO_FRAME_CALLBACK_REQUESTER_IMPL_H_

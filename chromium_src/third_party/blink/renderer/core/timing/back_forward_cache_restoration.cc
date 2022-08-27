1efba0ae2dab61a989887f0a107ba09ff6cadc89
Arthur Edelstein <arthuredelstein@gmail.com>
2022-08-26 18:23:26 -0700

    back_forward_cache_restoration

---
+++ chromium_src/third_party/blink/renderer/core/timing/back_forward_cache_restoration.cc
@@ -0,0 +1,19 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "third_party/blink/renderer/core/timing/back_forward_cache_restoration.h"
+#include "base/time/time.h"
+
+#include "src/third_party/blink/renderer/core/timing/back_forward_cache_restoration.cc"
+
+namespace blink {
+
+void LargestContentfulPaint::RoundOffTimes() {
+  page_show_event_start_ = round(page_show_event_start_);
+  page_show_event_end_ = round(page_show_event_end_);
+  PerformanceEntry::RoundOffTimes();
+}
+
+}  // namespace blink

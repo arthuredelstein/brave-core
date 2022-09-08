084b2a98979d3236cd416ba222f12638b4e3ae65
Arthur Edelstein <arthuredelstein@gmail.com>
2022-09-08 10:21:44 -0700

    xr_plane

---
+++ chromium_src/third_party/blink/renderer/modules/xr/xr_plane.cc
@@ -0,0 +1,25 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
+
+#include "third_party/blink/renderer/modules/xr/xr_plane.h"
+
+#define lastChangedTime lastChangedTime_ChromiumImpl
+
+#include "src/third_party/blink/renderer/modules/xr/xr_plane.cc"
+
+#undef lastChangedTime
+
+namespace blink {
+
+double XRPlane::lastChangedTime() const {
+  const double rawLastChangedTime = lastChangedTime_ChromiumImpl();
+  const bool allow_fingerprinting =
+      brave::AllowFingerprinting(session_->GetExecutionContext());
+  return allow_fingerprinting ? rawLastChangedTime : round(rawLastChangedTime);
+}
+
+}  // namespace blink
---
+++ chromium_src/third_party/blink/renderer/modules/xr/xr_plane.h
@@ -0,0 +1,18 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_XR_XR_PLANE_H_
+
+#define BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_XR_XR_PLANE_H_
+
+#define lastChangedTime                 \
+  lastChangedTime_ChromiumImpl() const; \
+  double lastChangedTime
+
+#include "src/third_party/blink/renderer/modules/xr/xr_plane.h"
+
+#undef lastChangedTime
+
+#endif  // BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_XR_XR_PLANE_H_

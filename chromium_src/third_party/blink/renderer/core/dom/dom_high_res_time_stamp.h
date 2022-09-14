51c372423a168a0c807d6b85e6fb7bb366d8c41d
Arthur Edelstein <aedelstein@brave.com>
2022-09-13 22:55:07 -0700

    Allow rounding of DOMHighResTimeStamp API values

---
+++ chromium_src/third_party/blink/renderer/core/dom/dom_high_res_time_stamp.h
@@ -0,0 +1,40 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_DOM_DOM_HIGH_RES_TIME_STAMP_H_
+
+#define BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_DOM_DOM_HIGH_RES_TIME_STAMP_H_
+
+#define DOMHighResTimeStamp DOMHighResTimeStamp_ChromiumImpl
+
+#define ConvertDOMHighResTimeStampToSeconds ConvertDOMHighResTimeStampToSeconds_ChromiumImpl
+
+#include "src/third_party/blink/renderer/core/dom/dom_high_res_time_stamp.h"
+
+#undef DOMHighResTimeStamp
+#undef ConvertDOMHighResTimeStampToSeconds
+
+namespace blink {
+
+class DOMHighResTimeStamp {
+public:
+  constexpr DOMHighResTimeStamp(int rhs): value_(rhs) { }
+  constexpr DOMHighResTimeStamp(double rhs): value_(rhs) { }
+  operator double() const { return getValue(); }
+  void operator=(double rhs) { value_ = rhs; }
+  void operator=(int rhs) { value_ = rhs; }
+  double getValue() const { return value_; }
+private:
+  double value_;
+};
+
+inline double ConvertDOMHighResTimeStampToSeconds(
+    DOMHighResTimeStamp milliseconds) {
+  return ConvertDOMHighResTimeStampToSeconds_ChromiumImpl(milliseconds);
+}
+
+}  // namespace blink
+
+#endif  // BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_DOM_DOM_HIGH_RES_TIME_STAMP_H_

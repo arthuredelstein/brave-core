e9895f3cb31e5970fe1efee44f14a08a1b67bf14
Arthur Edelstein <aedelstein@brave.com>
2022-09-19 17:15:07 -0700

    Round of all DOMHighResTimeStamps

---
+++ chromium_src/third_party/blink/renderer/core/dom/frame_request_callback_collection.cc
@@ -0,0 +1,16 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "third_party/blink/renderer/core/dom/frame_request_callback_collection.h"
+
+#include "third_party/blink/renderer/core/timing/time_clamper.h"
+
+#define InvokeAndReportException(ARG0, ARG_HIGHRESTIME) \
+  InvokeAndReportException(                             \
+      ARG0, TimeClamper::MaybeRoundMilliseconds(ARG_HIGHRESTIME))
+
+#include "src/third_party/blink/renderer/core/dom/frame_request_callback_collection.cc"
+
+#undef InvokeAndReportException

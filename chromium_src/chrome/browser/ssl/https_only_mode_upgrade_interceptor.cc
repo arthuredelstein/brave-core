81536d00870c4e690d9d152ce37f960c7e96dc03
Arthur Edelstein <arthuredelstein@gmail.com>
2022-09-19 18:17:25 -0700

    Enable HTTPS-Only Mode on Private Windows with Tor
    Create a feature flag, BraveTorWindowsHttpsOnly, that
    enables HTTPS-Only Mode for Private Windows with Tor.
    Enable that flag by default.
    
    Do not enforce HTTPS-Only for .onion sites: these
    are already secure when using http://.
    
    (Users can check "Always use secure connections" to enable
    HTTPS-Only Mode for other windows if they wish.)

---
+++ chromium_src/chrome/browser/ssl/https_only_mode_upgrade_interceptor.cc
@@ -0,0 +1,20 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "chrome/browser/ssl/https_only_mode_upgrade_interceptor.h"
+
+#include "net/base/url_util.cc"
+
+namespace {
+  bool IsOnion(const GURL& url) {
+    return net::IsSubdomainOf(url.host(), "onion");
+  }
+}  // namespace
+
+#define IsLocalhost(X) IsLocalhost(X) && !IsOnion(X)
+
+#include "src/chrome/browser/ssl/https_only_mode_upgrade_interceptor.cc"
+
+#undef IsLocalHost

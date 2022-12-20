288dec77f07a508f4ecf9606618999765e59700a
Arthur Edelstein <aedelstein@brave.com>
2022-12-19 23:33:56 -0800

    fix typo

--- chromium_src/chrome/browser/ui/webui/settings/site_settings_helper.cc
+++ chromium_src/chrome/browser/ui/webui/settings/site_settings_helper.cc
@@ -26,7 +26,7 @@
   {ContentSettingsType::BRAVE_SPEEDREADER, nullptr},               \
   {ContentSettingsType::BRAVE_ETHEREUM, nullptr},                  \
   {ContentSettingsType::BRAVE_SOLANA, nullptr},                    \
-  {ContentSettingsType::BRAVE_HTTPS_UPGRADE, nullptr},             \
+  {ContentSettingsType::BRAVE_HTTPS_UPGRADE, nullptr}
 // clang-format on
 
 #define BRAVE_SITE_SETTINGS_HELPER_CONTENT_SETTINGS_TYPE_FROM_GROUP_NAME \

16a2e69815ff3ef411ea91f59898823e3a3301d2
Arthur Edelstein <arthuredelstein@gmail.com>
2023-12-20 16:16:52 -0800

    Demo DoH fallback to Wikimedia DNS

---
+++ chromium_src/net/dns/dns_client.cc
@@ -0,0 +1,36 @@
+/* Copyright (c) 2023 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "net/dns/dns_util.h"
+#include "net/dns/public/dns_over_https_server_config.h"
+
+#include <vector>
+
+namespace net {
+namespace {
+
+std::vector<DnsOverHttpsServerConfig> MaybeAddFallbackDohServer(
+    const std::vector<DnsOverHttpsServerConfig>& doh_servers) {
+  if (!doh_servers.empty()) {
+    return doh_servers;
+  }
+  std::vector<DnsOverHttpsServerConfig> fallback_doh_servers;
+  auto fallbackDohServer = DnsOverHttpsServerConfig::FromString(
+      "https://wikimedia-dns.org/dns-query");
+  if (fallbackDohServer.has_value()) {
+    fallback_doh_servers.push_back(fallbackDohServer.value());
+  }
+  return fallback_doh_servers;
+}
+
+}  // namespace
+}  // namespace net
+
+#define GetDohUpgradeServersFromNameservers(...) \
+  MaybeAddFallbackDohServer(GetDohUpgradeServersFromNameservers(__VA_ARGS__))
+
+#include "src/net/dns/dns_client.cc"
+
+#undef GetDohUpgradeServersFromNameservers

7bb56d7410c226493e9b8203c8f63266ee32e53c
Arthur Edelstein <arthuredelstein@gmail.com>
2024-01-25 06:32:41 -0800

    add menu item

---
+++ browser/resources/settings/brave_overrides/secure_dns_settings.ts
@@ -0,0 +1,23 @@
+// Copyright (c) 2024 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at https://mozilla.org/MPL/2.0/.
+
+import {RegisterPolymerTemplateModifications} from 'chrome://resources/brave/polymer_overriding.js'
+
+RegisterPolymerTemplateModifications({
+  'settings-secure-dns': (templateContent) => {
+    console.log('hello from settings-secure-dns');
+    // if DoH fallback is enabled
+    const dohProviderMenu = templateContent.getElementById('resolverSelect');
+    if (!dohProviderMenu) {
+      console.error('[Brave Settings Overrides] Could not find resolverSelect id on security page.')
+    } else {
+      const automaticWithFallbackOption = document.createElement("option");
+      automaticWithFallbackOption.setAttribute('value', 'automaticWithFallback');
+      automaticWithFallbackOption.innerText = 'OS default or Quad9';//loadTimeData.getString('automaticWithFallbackDescription');
+      dohProviderMenu.insertBefore(automaticWithFallbackOption, dohProviderMenu.firstChild);
+    }
+    // end if DoH fallback is enabled
+  }
+})

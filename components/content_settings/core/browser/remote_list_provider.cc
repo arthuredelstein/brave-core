66033b9ed601af3ca708fd75fa85fec546b4b599
Arthur Edelstein <arthuredelstein@gmail.com>
2024-05-12 10:28:42 -0700

    remote_list_provider files working

--- components/content_settings/core/browser/remote_list_provider.cc
+++ components/content_settings/core/browser/remote_list_provider.cc
@@ -2,3 +2,51 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/content_settings/core/browser/remote_list_provider.h"
+
+namespace content_settings {
+
+RemoteListProvider::RemoteListProvider() { }
+
+std::unique_ptr<RuleIterator> RemoteListProvider::GetRuleIterator(
+    ContentSettingsType content_type,
+    bool off_the_record,
+    const PartitionKey& partition_key) const {
+      // TODO
+  return nullptr;
+}
+
+std::unique_ptr<Rule> RemoteListProvider::GetRule(
+    const GURL& primary_url,
+    const GURL& secondary_url,
+    ContentSettingsType content_type,
+    bool off_the_record,
+    const PartitionKey& partition_key) const {
+      // TODO
+  return nullptr;
+}
+
+bool RemoteListProvider::SetWebsiteSetting(
+    const ContentSettingsPattern& primary_pattern,
+    const ContentSettingsPattern& secondary_pattern,
+    ContentSettingsType content_type,
+    base::Value&& value,
+    const ContentSettingConstraints& constraints,
+    const PartitionKey& partition_key) {
+  // RemoteListProvider is read-only.
+  return false;
+}
+
+void RemoteListProvider::ClearAllContentSettingsRules(
+    ContentSettingsType content_type,
+    const PartitionKey& partition_key) {
+  // RemoteListProvider is read-only.
+  }
+
+void RemoteListProvider::ShutdownOnUIThread(){
+
+}
+
+}  // namespace content_settings
+                                
--- components/content_settings/core/browser/remote_list_provider.h
+++ components/content_settings/core/browser/remote_list_provider.h
@@ -2,3 +2,43 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "components/content_settings/core/browser/content_settings_provider.h"
+//#include "components/content_settings/core/browser/content_settings_observable_provider.h"
+
+namespace content_settings {
+
+class RemoteListProvider : public ProviderInterface {
+ public:
+  RemoteListProvider();
+  ~RemoteListProvider() override = default;
+
+  RemoteListProvider(const RemoteListProvider&) = delete;
+  RemoteListProvider& operator=(const RemoteListProvider&) = delete;
+
+  std::unique_ptr<RuleIterator> GetRuleIterator(
+      ContentSettingsType content_type,
+      bool off_the_record,
+      const PartitionKey& partition_key) const override;
+
+  std::unique_ptr<Rule> GetRule(
+      const GURL& primary_url,
+      const GURL& secondary_url,
+      ContentSettingsType content_type,
+      bool off_the_record,
+      const PartitionKey& partition_key) const override;
+
+  bool SetWebsiteSetting(const ContentSettingsPattern& primary_pattern,
+                         const ContentSettingsPattern& secondary_pattern,
+                         ContentSettingsType content_type,
+                         base::Value&& value,
+                         const ContentSettingConstraints& constraints,
+                         const PartitionKey& partition_key) override;
+
+  void ClearAllContentSettingsRules(ContentSettingsType content_type,
+                                    const PartitionKey& partition_key) override;
+
+  void ShutdownOnUIThread() override;
+};
+
+}  // namespace content_settings

967f95f9921f3af6681c515b039366ea99ed884f
Arthur Edelstein <arthuredelstein@gmail.com>
2024-05-22 12:07:46 -0700

    remove include for nonexistent file

--- chromium_src/chrome/browser/content_settings/generated_notification_pref_unittest.cc
+++ chromium_src/chrome/browser/content_settings/generated_notification_pref_unittest.cc
@@ -3,8 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#include "chrome/browser/content_settings/generated_notification_pref_unittest.h"
-
 #define BRAVE_SETTING_SOURCE_REMOTE_LIST                          \
   case SETTING_SOURCE_REMOTE_LIST:                                \
     provider_type = HostContentSettingsMap::REMOTE_LIST_PROVIDER; \

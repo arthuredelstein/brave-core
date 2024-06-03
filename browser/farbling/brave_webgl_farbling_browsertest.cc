adca7f112acf82d0a35987ef8cbdefa0cb61d4e3
Arthur Edelstein <arthuredelstein@gmail.com>
2024-06-03 14:15:50 -0700

    make test android compatible

--- browser/farbling/brave_webgl_farbling_browsertest.cc
+++ browser/farbling/brave_webgl_farbling_browsertest.cc
@@ -164,8 +164,8 @@ IN_PROC_BROWSER_TEST_F(BraveWebGLFarblingBrowserTest,
     // Get the actual WebGL2 parameter values.
     SetFingerprintingDefault(domain);
     brave_shields::SetWebcompatFeatureSetting(
-      content_settings(), ContentSettingsType::BRAVE_WEBCOMPAT_WEBGL,
-      ControlType::ALLOW, url, nullptr);
+        content_settings(), ContentSettingsType::BRAVE_WEBCOMPAT_WEBGL,
+        ControlType::ALLOW, url, nullptr);
     ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url));
     std::vector<int64_t> real_values2 =
         SplitStringAsInts(EvalJs(contents(), kTitleScript).ExtractString());

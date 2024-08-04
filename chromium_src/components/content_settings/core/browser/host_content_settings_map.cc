fd40d34daadaaee10753a73c2fcb7fca9b916bc3
Arthur Edelstein <arthuredelstein@gmail.com>
2024-08-04 12:48:35 -0700

    Delete pref-based webcompat setting when it matches remote setting
    Check pref-based settings whenever:
    1. The remote list changes
    2. A webcompat pref-setting changes
    
    If the pref-based webcompat exception setting is the same as the
    setting from the remote list, we delete the pref.

--- chromium_src/components/content_settings/core/browser/host_content_settings_map.cc
+++ chromium_src/components/content_settings/core/browser/host_content_settings_map.cc
@@ -6,10 +6,16 @@
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 
 #include "base/containers/contains.h"
+#include "base/trace_event/trace_event.h"
 #include "brave/components/content_settings/core/browser/remote_list_provider.h"
+#include "brave/components/webcompat/content/browser/webcompat_exceptions_service.h"
 #include "build/build_config.h"
+#include "components/content_settings/core/browser/content_settings_policy_provider.h"
+#include "components/content_settings/core/browser/content_settings_provider.h"
 #include "components/content_settings/core/browser/content_settings_utils.h"
+#include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/common/content_settings_types.h"
+#include "components/content_settings/core/common/content_settings_utils.h"
 #include "components/content_settings/core/common/features.h"
 
 #if !BUILDFLAG(IS_IOS)
@@ -53,9 +59,57 @@ bool IsMorePermissive_BraveImpl(ContentSettingsType content_type,
 
 #define IsMorePermissive(a, b) IsMorePermissive_BraveImpl(content_type, a, b)
 
+// Insert code into HostContentSettingsMap::ShutdownOnUIThread() body:
+#define clear() \
+  clear();      \
+  webcompat::WebcompatExceptionsService::RemoveObserver(this)
+
 #include "src/components/content_settings/core/browser/host_content_settings_map.cc"
 
+#undef clear
 #undef IsMorePermissive
+
+void HostContentSettingsMap::RemoveRedundantWebcompatSettingsByType(
+    ContentSettingsType settings_type) {
+  auto* svc = webcompat::WebcompatExceptionsService::GetInstance();
+  if (!svc) {
+    return;
+  }
+  const auto& patterns = svc->GetPatterns(settings_type);
+  for (const ContentSettingPatternSource& setting :
+       GetSettingsForOneType(settings_type)) {
+    if (setting.source == ProviderType::kPrefProvider) {
+      const auto prefSettingValue =
+          content_settings::ValueToContentSetting(setting.setting_value);
+      bool patternExists = std::find(patterns.begin(), patterns.end(),
+                                     setting.primary_pattern) != patterns.end();
+      if ((prefSettingValue == CONTENT_SETTING_BLOCK ||
+           prefSettingValue == CONTENT_SETTING_ASK) &&
+          !patternExists) {
+        SetContentSettingCustomScope(setting.primary_pattern,
+                                     ContentSettingsPattern::Wildcard(),
+                                     settings_type, CONTENT_SETTING_DEFAULT);
+      }
+    }
+  }
+}
+
+// Removes all webcompat settings set by user in Prefs that are the same as
+// those provided by the remote webcompat exceptions list.
+void HostContentSettingsMap::RemoveRedundantWebcompatSettings() {
+  for (auto settings_type = ContentSettingsType::BRAVE_WEBCOMPAT_NONE;
+       settings_type != ContentSettingsType::BRAVE_WEBCOMPAT_ALL;
+       settings_type = static_cast<ContentSettingsType>(
+           static_cast<int32_t>(settings_type) + 1)) {
+    RemoveRedundantWebcompatSettingsByType(settings_type);
+  }
+  RemoveRedundantWebcompatSettingsByType(
+      ContentSettingsType::BRAVE_FINGERPRINTING_V2);
+}
+
+void HostContentSettingsMap::OnWebcompatRulesUpdated() {
+  RemoveRedundantWebcompatSettings();
+}
 
 #if !BUILDFLAG(IS_IOS)
 #undef PrefProvider

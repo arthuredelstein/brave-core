cbe142d8dfd2ceea4274dd6027750d4f9fd04399
Arthur Edelstein <arthuredelstein@gmail.com>
2024-10-07 17:28:15 -0700

    performnace rounding

---
+++ chromium_src/third_party/blink/renderer/core/timing/performance.cc
@@ -0,0 +1,30 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "third_party/blink/renderer/core/timing/performance.h"
+
+#define now() now_ChromiumImpl()
+
+#include "src/third_party/blink/renderer/core/timing/performance.cc"
+
+#undef now
+
+#include "brave/third_party/blink/renderer/brave_farbling_constants.h"
+#include "brave/third_party/blink/renderer/core/farbling/brave_session_cache.h"
+
+namespace blink {
+
+DOMHighResTimeStamp Performance::now() const {
+        if (brave::GetBraveFarblingLevelFor(
+              GetExecutionContext(),
+              ContentSettingsType::BRAVE_WEBCOMPAT_PERFORMANCE,
+              BraveFarblingLevel::BALANCED) != BraveFarblingLevel::OFF) {
+                return 10 * now_ChromiumImpl()
+  } else {
+    return now_ChromiumImpl();
+  }
+}
+
+}  // namespace blink
---
+++ chromium_src/third_party/blink/renderer/core/timing/performance.h
@@ -0,0 +1,17 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_TIMING_PERFORMANCE_H_
+#define BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_TIMING_PERFORMANCE_H_
+
+#define now() \
+  now_ChromiumImpl() const; \
+  DOMHighResTimeStamp now()
+
+#include "src/third_party/blink/renderer/core/timing/performance.h"  // IWYU pragma: export
+
+#undef now
+
+#endif  // BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_TIMING_PERFORMANCE_H_

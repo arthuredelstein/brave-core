68433373a5a4807b36ea41a8894db8565c7730de
Arthur Edelstein <arthuredelstein@gmail.com>
2024-10-09 12:49:16 -0700

    performance_entry.h

---
+++ chromium_src/third_party/blink/renderer/core/timing/performance_entry.cc
@@ -0,0 +1,19 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "third_party/blink/renderer/core/timing/performance_entry.h"
+
+#include "brave/third_party/blink/renderer/core/farbling/brave_session_cache.h"
+
+#define duration_(...) \
+  duration_(brave::RoundPerformanceIfFarbling(DynamicTo<LocalDOMWindow>(source), __VA_ARGS__))
+
+#include "src/third_party/blink/renderer/core/timing/performance_entry.cc"
+
+#undef duration_
+
+namespace blink {
+
+}  // namespace blink

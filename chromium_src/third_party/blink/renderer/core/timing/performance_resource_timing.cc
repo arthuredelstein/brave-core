85245e12422166a431cfe2a5566b08b900f6144b
Arthur Edelstein <arthuredelstein@gmail.com>
2024-10-14 15:24:01 -0700

    progress

--- chromium_src/third_party/blink/renderer/core/timing/performance_resource_timing.cc
+++ chromium_src/third_party/blink/renderer/core/timing/performance_resource_timing.cc
@@ -5,11 +5,27 @@
 
 #include "third_party/blink/renderer/core/timing/performance_resource_timing.h"
 
+#include "brave/third_party/blink/renderer/core/farbling/brave_session_cache.h"
 #include "third_party/blink/renderer/core/timing/performance.h"
+#include "third_party/blink/renderer/core/timing/performance_entry.h"
 
-#define MonotonicTimeToDOMHighResTimeStamp(...) \
-  BraveMonotonicTimeToDOMHighResTimeStamp(GetExecutionContext(), __VA_ARGS__)
+#define MonotonicTimeToDOMHighResTimeStamp(...)                                \
+  BraveMonotonicTimeToDOMHighResTimeStamp(DynamicTo<LocalDOMWindow>(source()), \
+                                          __VA_ARGS__)
+
+// Put back the original MonotonicTimeToDOMHighResTimeStamp in the constructor.
+#define PerformanceEntry(PARAM1, PARAM2, PARAM3, PARAM4)                \
+  PerformanceEntry(                                                     \
+      PARAM1,                                                           \
+      Performance::MonotonicTimeToDOMHighResTimeStamp(                  \
+          time_origin, info->start_time, info->allow_negative_values,   \
+          cross_origin_isolated_capability),                            \
+      Performance::MonotonicTimeToDOMHighResTimeStamp(                  \
+          time_origin, info->response_end, info->allow_negative_values, \
+          cross_origin_isolated_capability),                            \
+      PARAM4)
 
 #include "src/third_party/blink/renderer/core/timing/performance_resource_timing.cc"
 
 #undef MonotonicTimeToDOMHighResTimeStamp
+#undef PerformanceEntry

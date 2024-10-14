339a7d8f2fe3a29971bcaa78a9b026b2f51c05a6
Arthur Edelstein <arthuredelstein@gmail.com>
2024-10-14 11:01:31 -0700

    performance.h override

--- chromium_src/third_party/blink/renderer/core/timing/performance.h
+++ chromium_src/third_party/blink/renderer/core/timing/performance.h
@@ -15,8 +15,19 @@ class DOMHighResTimeStamp;
   now_ChromiumImpl() const; \
   DOMHighResTimeStamp now()
 
+#define ClampTimeResolution(...) \
+  ClampTimeResolution(__VA_ARGS__); \
+  static DOMHighResTimeStamp BraveMonotonicTimeToDOMHighResTimeStamp( \
+    ExecutionContext* context, \
+    base::TimeTicks time_origin, \
+    base::TimeTicks monotonic_time, \
+    bool allow_negative_value, \
+    bool cross_origin_isolated_capability) \
+  );
+
 #include "src/third_party/blink/renderer/core/timing/performance.h"  // IWYU pragma: export
 
 #undef now
+#undef ClampTimeResolution
 
 #endif  // BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_CORE_TIMING_PERFORMANCE_H_

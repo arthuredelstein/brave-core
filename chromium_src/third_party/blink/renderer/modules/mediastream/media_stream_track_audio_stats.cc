16db48c9ed9fafcee0a39849613c53b7beb48fac
Arthur Edelstein <arthuredelstein@gmail.com>
2024-10-15 14:27:47 -0700

    MediaStreamTrackAudioStats

--- chromium_src/third_party/blink/renderer/modules/mediastream/media_stream_track_audio_stats.cc
+++ chromium_src/third_party/blink/renderer/modules/mediastream/media_stream_track_audio_stats.cc
@@ -5,8 +5,49 @@
 
 #include "third_party/blink/renderer/modules/mediastream/media_stream_track_audio_stats.h"
 
+#include "brave/third_party/blink/renderer/core/farbling/brave_session_cache.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_object_builder.h"
+#include "third_party/blink/renderer/platform/audio/audio_frame_stats_accumulator.h"
+
+#define deliveredFramesDuration deliveredFramesDuration_ChromiumImpl
+#define totalFramesDuration totalFramesDuration_ChromiumImpl
+#define latency latency_ChromiumImpl
+#define averageLatency averageLatency_ChromiumImpl
+#define minimumLatency minimumLatency_ChromiumImpl
+#define maximumLatency maximumLatency_ChromiumImpl
+
+#define AddNumber(A, B)                           \
+  AddNumber(A, brave::RoundPerformanceIfFarbling( \
+                   ExecutionContext::From(script_state), B))
+
 #include "src/third_party/blink/renderer/modules/mediastream/media_stream_track_audio_stats.cc"
 
+#undef deliveredFramesDuration
+#undef totalFramesDuration
+#undef latency
+#undef averageLatency
+#undef minimumLatency
+#undef maximumLatency
+
+#undef AddNumber
+
 namespace blink {
+
+#define DEFINE_ROUNDED_FUNC(FUNC_NAME)                       \
+  DOMHighResTimeStamp MediaStreamTrackAudioStats::FUNC_NAME( \
+      ScriptState* script_state) {                           \
+    return brave::RoundPerformanceIfFarbling(                \
+        ExecutionContext::From(script_state),                \
+        FUNC_NAME##_ChromiumImpl(script_state));             \
+  }
+
+DEFINE_ROUNDED_FUNC(deliveredFramesDuration)
+DEFINE_ROUNDED_FUNC(totalFramesDuration)
+DEFINE_ROUNDED_FUNC(latency)
+DEFINE_ROUNDED_FUNC(averageLatency)
+DEFINE_ROUNDED_FUNC(minimumLatency)
+DEFINE_ROUNDED_FUNC(maximumLatency)
+
+#undef DEFINE_ROUNDED_FUNC
 
 }  // namespace blink
--- chromium_src/third_party/blink/renderer/modules/mediastream/media_stream_track_audio_stats.h
+++ chromium_src/third_party/blink/renderer/modules/mediastream/media_stream_track_audio_stats.h
@@ -6,6 +6,39 @@
 #ifndef BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_MEDIA_STREAM_TRACK_AUDIO_STATS_H_
 #define BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_MEDIA_STREAM_TRACK_AUDIO_STATS_H_
 
+#include "third_party/blink/renderer/platform/audio/audio_frame_stats_accumulator.h"
+
+#define deliveredFramesDuration(...)    \
+  deliveredFramesDuration(__VA_ARGS__); \
+  DOMHighResTimeStamp deliveredFramesDuration_ChromiumImpl(__VA_ARGS__)
+
+#define totalFramesDuration(...)    \
+  totalFramesDuration(__VA_ARGS__); \
+  DOMHighResTimeStamp totalFramesDuration_ChromiumImpl(__VA_ARGS__)
+
+#define latency(...)    \
+  latency(__VA_ARGS__); \
+  DOMHighResTimeStamp latency_ChromiumImpl(__VA_ARGS__)
+
+#define averageLatency(...)    \
+  averageLatency(__VA_ARGS__); \
+  DOMHighResTimeStamp averageLatency_ChromiumImpl(__VA_ARGS__)
+
+#define minimumLatency(...)    \
+  minimumLatency(__VA_ARGS__); \
+  DOMHighResTimeStamp minimumLatency_ChromiumImpl(__VA_ARGS__)
+
+#define maximumLatency(...)    \
+  maximumLatency(__VA_ARGS__); \
+  DOMHighResTimeStamp maximumLatency_ChromiumImpl(__VA_ARGS__)
+
 #include "src/third_party/blink/renderer/modules/mediastream/media_stream_track_audio_stats.h"  // IWYU pragma: export
+
+#undef deliveredFramesDuration
+#undef totalFramesDuration
+#undef latency
+#undef averageLatency
+#undef minimumLatency
+#undef maximumLatency
 
 #endif  // BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_MEDIA_STREAM_TRACK_AUDIO_STATS_H_

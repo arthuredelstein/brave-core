240fd7ebd5ecb4550338e46a4cbd25301ee5c0c6
Arthur Edelstein <arthuredelstein@gmail.com>
2024-10-15 13:45:02 -0700

    media_stream_track_audio_stats

---
+++ chromium_src/third_party/blink/renderer/modules/mediastream/media_stream_track_audio_stats.h
@@ -0,0 +1,11 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_MEDIA_STREAM_TRACK_AUDIO_STATS_H_
+#define BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_MEDIA_STREAM_TRACK_AUDIO_STATS_H_
+
+#include "src/third_party/blink/renderer/modules/mediastream/media_stream_track_audio_stats.h"  // IWYU pragma: export
+
+#endif  // BRAVE_CHROMIUM_SRC_THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_MEDIA_STREAM_TRACK_AUDIO_STATS_H_

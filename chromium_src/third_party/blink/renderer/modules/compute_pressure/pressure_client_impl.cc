fad75e9f799e04cd868975dd1de86bf6e9990b7e
Arthur Edelstein <arthuredelstein@gmail.com>
2024-10-16 07:06:30 -0700

    pressure_client_impl.cc

---
+++ chromium_src/third_party/blink/renderer/modules/compute_pressure/pressure_client_impl.cc
@@ -0,0 +1,12 @@
+/* Copyright (c) 2024 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "third_party/blink/renderer/modules/compute_pressure/pressure_client_impl.h"
+
+#include "src/third_party/blink/renderer/modules/compute_pressure/pressure_client_impl.cc"
+
+namespace blink {
+
+}  // namespace blink

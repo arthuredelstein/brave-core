7d98278bdda729af732099dcf7e093c65d4614ca
Arthur Edelstein <arthuredelstein@gmail.com>
2024-10-16 22:34:50 -0700

    thingie

---
+++ inbox_aliases/resources/shared/components/settings/stories/index.tsx
@@ -0,0 +1,34 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+ import * as React from 'react'
+ import styled from 'styled-components'
+ import * as knobs from '@storybook/addon-knobs'
+ import MainPanel from '../components/main-panel'
+ 
+ 
+ export default {
+   title: 'Inbox Aliases/Main'
+ }
+ 
+ function actionLogger (name: string, ...args: any[]) {
+   return (...args: any[]) => console.log(name, ...args)
+ }
+ 
+ const style = {
+   card: styled.div`
+     width: 284px;
+     background: #1C1E26B2;
+     backdrop-filter: blur(27.5px);
+     border-radius: 16px;
+     padding: 24px;
+   `
+ }
+ 
+ export const _Main = () => {
+  return (
+    <div>hello</div>
+  )
+}

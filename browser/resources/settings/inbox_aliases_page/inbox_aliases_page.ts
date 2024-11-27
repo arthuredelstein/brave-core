ed4ba67d77d8c003ac28f4cda4e495b9d569ffdf
Arthur Edelstein <aedelstein@brave.com>
2024-11-26 19:15:08 -0800

    Revert "strings to 'email aliases'"
    This reverts commit 4716fc8623b8f93f99c3b38c4efc2ade1b2279ef.

--- browser/resources/settings/inbox_aliases_page/inbox_aliases_page.ts
+++ browser/resources/settings/inbox_aliases_page/inbox_aliases_page.ts
@@ -17,6 +17,8 @@ class InboxAliasesPage extends HTMLElement {
   connectedCallback() {
     this.attachShadow({ mode: 'open' })
 
+    console.log("Hello from InboxAliasesPage")
+
     import('/inbox_aliases.bundle.js' as any)
       .then(() => (window as any).mountInboxAliases(this.shadowRoot))
       .catch(() => {})

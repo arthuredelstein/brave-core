5509957a46d4228dd2429fbe6e016c041bdee55a
Arthur Edelstein <aedelstein@brave.com>
2025-02-17 17:59:05 -0800

    popup item using a leading icon

---
+++ chromium_src/chrome/browser/ui/views/autofill/popup/popup_row_factory_utils.cc
@@ -0,0 +1,13 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "chrome/browser/ui/views/autofill/popup/popup_row_factory_utils.h"
+
+#define kManagePlusAddress \
+  kNewEmailAlias, SuggestionType::kManagePlusAddress
+
+#include "src/chrome/browser/ui/views/autofill/popup/popup_row_factory_utils.cc"
+
+#undef kManagePlusAddress

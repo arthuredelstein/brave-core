10f0b0f5e7d81e3e1523daa13625947c5f712c6d
Arthur Edelstein <aedelstein@brave.com>
2025-06-04 16:15:32 -0700

    client_hints_contoller_delegate
    more BraveGetUserAgentMetadata
    
    hide brave brand when showBraveBrand == false

---
+++ chromium_src/components/client_hints/browser/client_hints.cc
@@ -0,0 +1,36 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "components/client_hints/browser/client_hints.h"
+
+#include <iostream>
+
+#include "src/components/client_hints/browser/client_hints.cc"
+
+namespace client_hints {
+
+namespace {
+constexpr char kGoogleChromeBrand[] = "Google Chrome";
+}  // namespace
+
+blink::UserAgentMetadata ClientHints::BraveGetUserAgentMetadata(
+    bool showBraveBrand) {
+  std::cout << "ClientHints::BraveGetUserAgentMetadata"
+            << ", showBraveBrand=" << showBraveBrand << std::endl;
+  auto metadata = GetUserAgentMetadata();
+  if (!showBraveBrand) {
+    for (auto& brand_version : metadata.brand_version_list) {
+      std::cout << "brand=" << brand_version.brand
+                << ", version=" << brand_version.version << std::endl;
+      if (brand_version.brand == "Brave") {
+        brand_version.brand = kGoogleChromeBrand;
+        break;
+      }
+    }
+  }
+  return metadata;
+}
+
+}  // namespace client_hints
---
+++ chromium_src/content/browser/client_hints/client_hints.cc
@@ -0,0 +1,15 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "content/browser/client_hints/client_hints.h"
+
+#include "content/public/browser/client_hints_controller_delegate.h"
+#include "content/public/browser/content_browser_client.h"
+
+#define GetUserAgentMetadata() BraveGetUserAgentMetadata(false)
+
+#include "src/content/browser/client_hints/client_hints.cc"
+
+#undef GetUserAgentMetadata

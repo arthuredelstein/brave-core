242f99ed2784087905a403ad2870d5603c7282ab
Arthur Edelstein <aedelstein@brave.com>
2025-10-13 15:44:07 -0700

    manage button

--- browser/resources/settings/email_aliases_page/content/email_aliases_modal.tsx
+++ browser/resources/settings/email_aliases_page/content/email_aliases_modal.tsx
@@ -297,6 +297,10 @@ export const EmailAliasModal = (
       }
       <ButtonRow bubble={bubble}>
         <span>
+          {bubble &&
+            <Button onClick={() => { window.open('brave://settings/email-aliases'); onReturnToMain(undefined) }} kind='plain'>
+              {getLocale('emailAliasesManageButton')}
+            </Button>}
           <Button onClick={() => onReturnToMain(undefined)} kind='plain-faint'>
             {getLocale('emailAliasesCancelButton')}
           </Button>
